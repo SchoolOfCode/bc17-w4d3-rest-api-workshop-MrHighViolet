9c0a705f5a27eb5e2a7b91b587d1ea5c2b9bd505
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-07-23 19:37:52 +0100

    done 3

--- app.js
+++ app.js
@@ -30,15 +30,47 @@ res.json({
 /* Write a request handler to return the correct response when a `GET` request is received to `/astronauts`. Choose the appropriate 
 function from the imported functions at the top of the `app.js` to get your data. */
 
+app.get('/astronauts', async (req, res) => {
+  const data = await getAstronauts();
+  
+  const astronauts = {
+    success: true,
+    payload: data
+  };
+  res.json(astronauts);
+});
+
 // Task 2
 
 /* Write a request handler to return the correct response and perform the correct action when a `POST` request is received to 
 `/astronauts`. Choose the appropriate function from the imported functions at the top of the `app.js` to perform the action. */
 
+app.post('/astronauts', async (req, res) => {
+  const update = await createAstronaut(req.body);
+
+  const newAstronauts = {
+    success: true,
+    payload: update
+  };
+  res.json(newAstronauts);
+});
+
 // Task 3
 
 /* Write the request handler to return the data from the function getAstronautById. Have this handler listen to requests at the 
 appropriate path. */
+
+app.get('/astronauts/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  const astroById = await getAstronautById(id);
+  
+  const boom = {
+    success: true,
+    payload: astroById
+  };
+  res.json(boom);
+});
 
 // Task 4
 
